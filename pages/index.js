b1e3df63a0461583ab7264da0ab9c269cabf3125
NaMachadoG <namachadog@hotmail.com>
2026-01-07 19:15:23 +0000

    modificação da frase original

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Você é meu lar 🥰</h1>
+    return <h1>Amor,
+eu só quero que você saiba o quanto eu te amo e o quanto sou grata por ter você na minha vida. Você cuida de mim de uma forma que me faz sentir segura, amada e acolhida todos os dias. O carinho, a atenção e o cuidado que você tem comigo, com o Aquiles e o Ryuk, dizem muito sobre quem você é como homem e como pessoa.
+Tenho um orgulho enorme de você. Vejo o quanto você se dedica ao trabalho, o quanto se esforça e faz de tudo para nos dar uma vida melhor, mesmo quando cansa, mesmo quando é difícil. Sua força, responsabilidade e amor pela nossa família me inspiram demais.
+Obrigada por ser esse parceiro incrível, esse exemplo de cuidado e amor. Caminhar ao seu lado é um privilégio, e eu tenho certeza de que estamos construindo algo lindo juntos. Te amo hoje e sempre. 💙🐾🥰</h1>
 }
 
 export default Home;
